a10e69a2d4bbed98be12cac54c271e94639132a9
LuukEbenau <45255779+LuukEbenau@users.noreply.github.com>
2024-05-27 15:26:07 +0200

    initial commit

--- src/main.py
+++ src/main.py
@@ -1,4 +1,20 @@
-# TODO: make some baseline code
-from vocoder import vocode
 
-print(vocode())
+
+def parse_args():
+	parser = argparse.ArgumentParser()
+	# Do our argument parsing code here
+	args = parser.parse_args()
+	return args
+
+# Main code
+def main(args):
+	# TODO: make some baseline code
+	from vocoder import vocode
+
+	print(vocode())
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
+
+
